c3d6f3e047b23ec56bef2d492dda1ffdc93649e2
Irfaan <irfaankhodabukus@gmail.com>
2018-05-21 12:53:54 +0200

    add navigation and data

---
+++ src/data/artists.js
@@ -0,0 +1,47 @@
+export const artists = [
+    {
+    id: 1,
+    photo: 'https://media.licdn.com/dms/image/C4E03AQHZ5kAxawJlog/profile-displayphoto-shrink_200_200/0?e=1532563200&v=beta&t=Ota0KmS0laP6SIUqlfU1AVGMPIkEt4Mse7ojXv7gWXE',
+    name: 'TeeJay',
+    email: 'teejay@gmail.com',
+    phone: 123456767,
+    skills: ['Singer', 'Lyricist', 'Rapper'],
+    track: ['audio.mp3', 'audio2.mp3', 'audio3.mp3']
+    },
+    {
+    id: 2,
+    photo: 'https://media.licdn.com/dms/image/C4E03AQHZ5kAxawJlog/profile-displayphoto-shrink_200_200/0?e=1532563200&v=beta&t=Ota0KmS0laP6SIUqlfU1AVGMPIkEt4Mse7ojXv7gWXE',
+    name: 'TeeJay',
+    email: 'teejay@gmail.com',
+    phone: 123456767,
+    skills: ['Singer', 'Lyricist', 'Rapper'],
+    track: ['audio.mp3', 'audio2.mp3', 'audio3.mp3']
+    },
+    {
+    id: 3,
+    photo: 'https://media.licdn.com/dms/image/C4E03AQHZ5kAxawJlog/profile-displayphoto-shrink_200_200/0?e=1532563200&v=beta&t=Ota0KmS0laP6SIUqlfU1AVGMPIkEt4Mse7ojXv7gWXE',
+    name: 'TeeJay',
+    email: 'teejay@gmail.com',
+    phone: 123456767,
+    skills: ['Singer', 'Lyricist', 'Rapper'],
+    track: ['audio.mp3', 'audio2.mp3', 'audio3.mp3']
+    },
+    {
+    id: 4,
+    photo: 'https://media.licdn.com/dms/image/C4E03AQHZ5kAxawJlog/profile-displayphoto-shrink_200_200/0?e=1532563200&v=beta&t=Ota0KmS0laP6SIUqlfU1AVGMPIkEt4Mse7ojXv7gWXE',
+    name: 'TeeJay',
+    email: 'teejay@gmail.com',
+    phone: 123456767,
+    skills: ['Singer', 'Lyricist', 'Rapper'],
+    track: ['audio.mp3', 'audio2.mp3', 'audio3.mp3']
+    },
+    {
+    id: 5,
+    photo: 'https://media.licdn.com/dms/image/C4E03AQHZ5kAxawJlog/profile-displayphoto-shrink_200_200/0?e=1532563200&v=beta&t=Ota0KmS0laP6SIUqlfU1AVGMPIkEt4Mse7ojXv7gWXE',
+    name: 'TeeJay',
+    email: 'teejay@gmail.com',
+    phone: 123456767,
+    skills: ['Singer', 'Lyricist', 'Rapper'],
+    track: ['audio.mp3', 'audio2.mp3', 'audio3.mp3']
+    }
+];
